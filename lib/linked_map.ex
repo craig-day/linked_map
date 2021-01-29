13267a5bfe08e2d0cc73d1ee06d11976213c8e1e
Craig Day <cday@zendesk.com>
2021-01-29 11:40:33 -0800

    mix new

---
+++ lib/linked_map.ex
@@ -0,0 +1,18 @@
+defmodule LinkedMap do
+  @moduledoc """
+  Documentation for `LinkedMap`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LinkedMap.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/linked_map_test.exs
@@ -0,0 +1,8 @@
+defmodule LinkedMapTest do
+  use ExUnit.Case
+  doctest LinkedMap
+
+  test "greets the world" do
+    assert LinkedMap.hello() == :world
+  end
+end
